dce80c390ab58099500265c923d5ddc043af8c8c
Kacper Mentel <kacpermentel@MacBook-Pro-Kacper.local>
2020-04-04 22:51:41 +0200

    Echo data when the new line character is detected

--- lib/receiver.ex
+++ lib/receiver.ex
@@ -1,5 +1,6 @@
 defmodule EchoServex.Receiver do
   use GenServer, restart: :temporary
+  require Logger
 
   def start_link(socket) do
     GenServer.start_link(__MODULE__, socket)
@@ -8,15 +9,21 @@ defmodule EchoServex.Receiver do
   @impl true
   def init(socket) do
     :inet.setopts(socket, [{:active, :once}])
-    {:ok, %{socket: socket}}
+    {:ok, %{socket: socket, chunks: []}}
   end
 
   @impl true
-  def handle_info({:tcp, socket, data}, %{socket: socket} = state) do
-    IO.inspect data
-    :gen_tcp.send(socket, data)
-    :inet.setopts(socket, [{:active, :once}])
-    {:noreply, state}
+  def handle_info({:tcp, socket, data}, %{socket: socket, chunks: chunks} = state) do
+    case String.last(data) do
+      "\n" ->
+        complete_data = List.foldr(chunks, "", &(&2 <> &1)) <> data
+        Logger.info(complete_data)
+        :gen_tcp.send(socket, complete_data)
+        {:noreply, state}
+      _ ->
+        :inet.setopts(socket, [{:active, :once}])
+        {:noreply, %{state | chunks: [data | chunks]}}
+    end
   end
   def handle_info({:tcp_closed, socket}, %{socket: socket} = state) do
     {:stop, :tcp_closed, state}
